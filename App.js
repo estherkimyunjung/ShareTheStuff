e790116763fd58226c28050e062150f57d46ac5b
estherkimyunjung <estherkimyunjung@gmail.com>
2021-02-04 16:43:25 -0600

    test screenview

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  SafeAreaView, 
+  Alert, 
+  Button, 
+  StatusBar, 
+  Platform 
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Esther!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container,containerStyle}>
+      <Button
+        color="green"
+        title="Hire Esther!!"
+        onPress={() => 
+          Alert.alert("Shopify", "I am ready", [
+            {text: "Yes", onPress: () => console.log("Yes")},
+            {text: "Hired", onPress: () => console.log("Hired")}
+          ])
+          // Alert.prompt("My title", "My Message",(text => console.log(text)))
+        }
+      />
+    </SafeAreaView>
   );
 }
+
+const containerStyle = {backgroundColor: "orange"}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     alignItems: 'center',
     justifyContent: 'center',
   },
